adbf2d6b073d107bc1e68ac545a4570a61b52a29
dsolis421 <danny@march76.com>
2018-08-23 22:13:34 -0500

    layout adjustments, about wording, font awesome added

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
 
   setTimeout(function(){
-    $('#moving-header').animate({"top": "0"}, 1000, "swing",function(){
+    $('#moving-header').animate({"top": "0"}, 2000, "swing",function(){
       setTimeout(function(){
         $('.entry-section, #fadein-footer').fadeIn(1000,"swing");
-        console.log("fadeIn fired");
-      }, 2000);
+        $('#moving-header .logo-container a').animate({"opacity": "1"}, 1000);
+      }, 200);
     });
   }, 500)
 });
