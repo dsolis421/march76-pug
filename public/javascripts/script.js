8cfc6cb89e20bffd9b306aea0d40a4e6e87f7c42
dsolis421 <danny@march76.com>
2018-09-09 19:37:04 -0500

    new images, responsive nav

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,11 +1,27 @@
+var $footernav = $('footer');
+
+function respNavToggle() {
+  if($footernav.attr('class') === "toggle-nav") {
+    $footernav.addClass('responsive');
+  } else {
+    $footernav.removeClass('responsive');
+  }
+}
+
 $(document).ready(function(){
 
   setTimeout(function(){
     $('#moving-header').animate({"top": "0"}, 2000, "swing",function(){
       setTimeout(function(){
         $('.entry-section, #fadein-footer').fadeIn(1000,"swing");
-        $('#moving-header .logo-container a').animate({"opacity": "1"}, 1000);
-      }, 200);
+        $('#moving-header > div span').css({"line-height": "50px"});
+        $('#moving-header .logo-container a').animate({"opacity": "1"}, 500);
+      }, 200)
     });
-  }, 500)
+  }, 500);
+
+  $('.icon').click(function() {
+    respNavToggle();
+  });
+
 });
