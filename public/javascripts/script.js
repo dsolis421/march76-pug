00d5d77ed066caf00ebc9dd18cc11b561a26c6fa
dsolis421 <danny@march76.com>
2018-11-11 12:50:03 -0600

    new splash layout

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -11,16 +11,17 @@ function respNavToggle() {
 $(document).ready(function(){
 
   setTimeout(function(){
-    $('#moving-header').animate({"top": "0"}, 2000, "swing",function(){
+    $('#entry-header').animate({"top": "0"}, 100, "swing",function(){
       setTimeout(function(){
         $('.entry-section, #fadein-footer').fadeIn(1000,"swing");
         if($(window).width() < 555){
-          $('#moving-header > div span').css({"line-height": "50px"});
+          $('#entry-header > div span').css({"line-height": "50px"});
         }
-        $('#moving-header .logo-container a').animate({"opacity": "1"}, 500);
-      }, 200)
+        $('#entry-header .logo-container a').animate({"opacity": "1"}, 500);
+        $('.entry-blend').css({"background": "rgba(0,0,0,.8)"});
+      }, 100)
     });
-  }, 500);
+  }, 200);
 
   $('.icon').click(function() {
     respNavToggle();
