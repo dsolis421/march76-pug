9a01cc505a98ae0bb60be20631a42b6bcae0c823
dsolis421 <danny@march76.com>
2019-08-19 19:36:01 -0500

    another redesign attempt

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,10 +1,16 @@
-var $footernav = $('footer');
+var $footernav = $('.expanda-nav > div');
 
 function respNavToggle() {
   if($footernav.attr('class') === "toggle-nav") {
     $footernav.addClass('responsive');
+    $('.expanda-nav').animate({"width":"100%"}, 600).css({"background":"rgba(0,0,0,.5)"});
+    $('.toggle-nav').animate({"width":"90%"},700);
+    $('.responsive a').fadeIn(800,"swing");
   } else {
     $footernav.removeClass('responsive');
+    $('.responsive a').fadeOut(600,"swing");
+    $('.toggle-nav').animate({"width":"0"}, 700);
+    $('.expanda-nav').animate({"width":"5%"}, 800).css({"background":"none"});
   }
 }
 
@@ -12,12 +18,8 @@ $(document).ready(function(){
 
   setTimeout(function(){
     $('.entry-blend').animate({"opacity": "1"}, 1000);
-    $('.entry-nav, #fadein-footer').fadeIn(1700,"swing");
-    if($(window).width() < 555){
-      $('#entry-header > div span').css({"line-height": "50px"});
-    }
-    $('#entry-header .header-social-container a').animate({"opacity": "1"}, 1700);
-    //$('.entry-blend').css({"background": "rgba(0,0,0,.7)"});
+    $('.entry-logo, #fadein-footer, #entry-header .icon').fadeIn(1700,"swing");
+    $('#fadein-footer *').animate({"opacity": "1"}, 1700);
   }, 700);
 
 
