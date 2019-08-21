c8206ec339ba17869bb5e0ceffd82887cb69e1d9
dsolis421 <danny@march76.com>
2019-08-20 20:50:23 -0500

    working expanding nav menu

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -3,14 +3,16 @@ var $footernav = $('.expanda-nav > div');
 function respNavToggle() {
   if($footernav.attr('class') === "toggle-nav") {
     $footernav.addClass('responsive');
-    $('.expanda-nav').animate({"width":"100%"}, 600).css({"background":"rgba(0,0,0,.5)"});
+    $('#entry-header .expanda-nav').animate({"width":"100%"}, 600).css({"background":"rgba(0,0,0,.5)"});
+    $('#static-header .expanda-nav').animate({"width":"100%"}, 600);
     $('.toggle-nav').animate({"width":"90%"},700);
-    $('.responsive a').fadeIn(800,"swing");
+    $('.toggle-nav a').fadeIn(800,"swing");
   } else {
     $footernav.removeClass('responsive');
-    $('.responsive a').fadeOut(600,"swing");
+    $('.toggle-nav a').fadeOut(600,"swing");
     $('.toggle-nav').animate({"width":"0"}, 700);
-    $('.expanda-nav').animate({"width":"5%"}, 800).css({"background":"none"});
+    $('#entry-header .expanda-nav').animate({"width":"5%"}, 800).css({"background":"none"});
+    $('#static-header .expanda-nav').animate({"width":"5%"}, 800);
   }
 }
 
@@ -18,7 +20,7 @@ $(document).ready(function(){
 
   setTimeout(function(){
     $('.entry-blend').animate({"opacity": "1"}, 1000);
-    $('.entry-logo, #fadein-footer, #entry-header .icon').fadeIn(1700,"swing");
+    $('.entry-logo, #fadein-footer, #entry-header .icon.fadein-icon').fadeIn(1700,"swing");
     $('#fadein-footer *').animate({"opacity": "1"}, 1700);
   }, 700);
 
