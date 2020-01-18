374126957c13f84c7663ed2bf2a54668c6638a29
dsolis421 <danny@march76.com>
2020-01-18 09:03:51 -0600

    cleaned up old code for previous gallery API calls

--- controllers/marController.js
+++ controllers/marController.js
@@ -5,33 +5,6 @@ const gallerypics = mongoose.model('gallerypics');
 const moodboard = mongoose.model('boards');
 const gallerycollection = mongoose.model('m76galleries');
 
-/*var pets = [];
-var portraits = [];
-var creative = [];*/
-
-//gets overall, shortened view of the gallery
-/*exports.getGallery = (req, res) => {
-  gallerypics.find({show: "y", category: req.params.cat}).sort({order: 1}).exec()
-  .then(images => {
-    var pics = images.length - 1;
-    var images1 = [];
-    var images2 = [];
-    for (var x = 0; x <= pics; x++) {
-      if (x <= (pics/2)) {
-        images1.push(images[x]);
-      }
-      else {
-        images2.push(images[x]);
-      }
-    }
-    //console.log(images1);
-    //console.log(images2);
-    res.render('gallerycategory', { title: 'march76 - ' + req.params.cat, images1, images2 });
-  })
-  .catch(err => {
-    next(err);
-  });
-}*/
 
 exports.getGalleries = (req, res) => {
   gallerycollection.find({show: "y"}, { name: 1, frontpageimage: 1, quick: 1}).sort({order: 1}).exec()
@@ -79,47 +52,6 @@ exports.getCollectionImages = (req, res, next) => {
   });
 }
 
-//gets a specific gallery for pets
-/*exports.getGalleryPets = (req, res, next) => {
-  gallerypics.find({show: "y", category: "Pets", frontpage: "y"}).exec()
-  .then(returnedPets => {
-    pets = returnedPets;
-    next();
-  })
-  .catch(err => {
-    next(err);
-  });
-}*/
-
-//gets a specific gallery for portraits
-/*exports.getGalleryPortraits = (req, res, next) => {
-  gallerypics.find({show: "y", category: "Portraits", frontpage: "y"}).exec()
-  .then(returnedPortraits => {
-    portraits = returnedPortraits;
-    next();
-  })
-  .catch(err => {
-    next(err);
-  });
-}*/
-
-//gets a specific gallery for creative images
-/*exports.getGalleryCreative = (req, res, next) => {
-  gallerypics.find({show: "y", category: "Creative", frontpage: "y"}).exec()
-  .then(returnedCreative => {
-    creative = returnedCreative;
-    res.render('gallerylanding', {title: 'march78 - Gallery', pets, portraits, creative});
-  })
-  .then(() => {
-    pets = [];
-    portraits = [];
-    creative = [];
-  })
-  .catch(err => {
-    next(err);
-  });
-}*/
-
 //gets a listing of mood boards for display
 exports.getBoardsList = (req, res, next) => {
   moodboard.find().sort({status: -1}).exec()
@@ -151,9 +83,7 @@ exports.getMoodboard = (req, res, next) => {
         collage2.push(board[0].samples[x]);
       }
     }
-    /*var palette = board[0].colorpalette;
-    var name = board[0].name;
-    var desc = board[0].desc.substring(0,50) + "...";*/
+    //TODO: make this more efficient
     project.name = board[0].name;
     project.quick = board[0].quick;
     project.palette = board[0].colorpalette;
