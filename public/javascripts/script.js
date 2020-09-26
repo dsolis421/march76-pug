809d50e46c1d9ea1cc59a48fcefa6d2720a80956
dsolis421 <danny@march76.com>
2020-09-26 16:08:48 -0500

    added functions for change of photoreq and click of submit, with post and clear form values

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,11 @@
 var $togglenav = $('.expanda-nav > .toggle-nav');
 
+function clearFormValues() {
+  $('form > input, form > textarea').val('');
+  $('select').val('none');
+  $('.contact-business-field').fadeOut();
+}
+
 function respNavToggle() {
   if($togglenav.hasClass('expanded')) {
     //$('.logo-container').fadeOut(400,"swing");
@@ -11,8 +17,22 @@ function respNavToggle() {
   }
 }
 
-$(document).ready(function(){
+function sendContactData(contactData) {
+  $.post("/contact/submit", contactData, function(data,status,xhr) {
+    if(status == "success") {
+      clearFormValues();
+      alert("Your message has been sent!");
+    } else if (status == "error") {
+      alert("Oops, something happened try again")
+    } else if (status == "timeout") {
+      alart("Our site appears to be down, try again later")
+    }
+  });
+}
 
+$(document).ready(function(){
+  clearFormValues()
+  
   /*setTimeout(function(){
     $('.entry-blend').animate({"opacity": "1"}, 1000);
     $('.entry-logo, #fadein-footer, #entry-header .icon.fadein-icon').fadeIn(1500,"swing");
@@ -30,4 +50,17 @@ $(document).ready(function(){
     }
   });
 
+  $('#contactformsend').click(function() {
+    console.log('clicked contact send');
+    var contactSendData = {
+      contactname: $('#contactname').val(),
+      contactemail: $('#contactemail').val(),
+      contactphone: $('#contactphone').val(),
+      contactphotoreq: $('#contactphotoreq').val(),
+      contactbusname: $('#contactbusname').val(),
+      contactbussite: $('#contactbussite').val(),
+      contacttext: $('#contacttext').val()
+    }
+    sendContactData(contactSendData);
+  });
 });
